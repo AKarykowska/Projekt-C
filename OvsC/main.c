588a6817b48c934cb2064865c9e913303b7c2e6a
AKarykowska <akarykowska00@gmail.com>
2020-01-29 22:46:05 +0100

    Connect menu with blank window

--- OvsC/main.c
+++ OvsC/main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
+GtkWidget *window;
 
+static void set_window (GtkWidget *menu, gpointer data)
+{
+    gtk_widget_hide(gtk_widget_get_parent_window(menu));
+    GtkWidget *test;
+    test = gtk_label_new("a kuku");
+    gtk_container_add(GTK_CONTAINER(window),test);
+    gtk_widget_show_all(window);
+}
 int main(int argc, char **argv)
 {
     gtk_init(&argc, &argv);
-    GtkWidget *window;
-    GtkWidget *menu_box, *menu_nowa_gra, *menu_wczytaj_zapis, *menu_komputer, *tytul;
+
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Order and Chaos");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    //dialog
+    /* GtkWidget *menu_dialog = gtk_message_dialog_new(GTK_WINDOW(window), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_QUESTION, GTK_BUTTONS_NONE, "Order and Chaos");
+    gtk_dialog_add_buttons(GTK_DIALOG(menu_dialog), u8"Nowa gra dla dwóch osób", GTK_RESPONSE_YES, "Wczytaj grę", GTK_RESPONSE_ACCEPT, "Nowa gra z komputerem", GTK_RESPONSE_NO, NULL);
+    int response = gtk_dialog_run(GTK_DIALOG(menu_dialog));
+    gtk_widget_destroy(menu_dialog); */
+
+    GtkWidget *menu;
+    GtkWidget *menu_box, *menu_nowa_gra, *menu_wczytaj_zapis, *menu_komputer, *tytul;
+
+    menu = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(menu), "Order and Chaos");
+    g_signal_connect(menu, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     menu_box = gtk_box_new(GTK_ORIENTATION_VERTICAL,0);
     menu_nowa_gra = gtk_button_new_with_label(u8"Nowa gra dla dwóch osób");
@@ -23,8 +44,13 @@ int main(int argc, char **argv)
     gtk_box_pack_start(GTK_BOX(menu_box),menu_wczytaj_zapis,1,1,1);
     gtk_box_pack_start(GTK_BOX(menu_box),menu_komputer,1,1,1);
 
-    gtk_container_add(GTK_CONTAINER(window),menu_box);
-    gtk_widget_show_all(window);
+    gtk_container_add(GTK_CONTAINER(menu),menu_box);
+    gtk_widget_show_all(menu);
+
+    g_signal_connect(menu_nowa_gra, "clicked", G_CALLBACK(set_window), "a kuku");
+    //g_signal_connect(menu_wczytaj_zapis, "clicked", G_CALLBACK(set_window);
+    //g_signal_connect(menu_komputer, "clicked", G_CALLBACK(set_window));
+
     gtk_main();
     return 0;
 }
