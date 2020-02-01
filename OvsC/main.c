d91ddd506868eb131a595a110723aeb2e2d194a7
AKarykowska <akarykowska00@gmail.com>
2020-02-02 00:22:30 +0100

    Draw board and two labels in main window. Change names to english

--- OvsC/main.c
+++ OvsC/main.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
+
 GtkWidget *window;
 GtkWidget *menu;
-GtkWidget *menu_box, *menu_nowa_gra, *menu_wczytaj_zapis, *menu_komputer, *tytul;
+GtkWidget *menu_box, *menu_new_game, *menu_load, *menu_ai, *title;
+
+GtkWidget *main_box; //pionowy
+GtkWidget *move; //label
+GtkWidget *board; //grid
+GtkWidget *color; //na razie label
+
+static void set_main_area(void)
+{
+    main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL,0);
+    move = gtk_label_new("TURA");
+    board = gtk_grid_new();
+    for (int x=0; x<6; x++)
+        for(int y=0; y<6; y++)
+        {
+            GtkWidget *child;
+            child = gtk_frame_new(NULL);
+            gtk_widget_set_size_request(child, 100, 100);
+            gtk_frame_set_shadow_type(GTK_FRAME(child), GTK_SHADOW_IN);
+            gtk_grid_attach(GTK_GRID(board), child, x, y, 1, 1);
+
+        }
+    color = gtk_label_new("KOLOR");
+    gtk_box_pack_start(GTK_BOX(main_box),move,1,1,1);
+    gtk_box_pack_start(GTK_BOX(main_box),board,1,1,1);
+    gtk_box_pack_start(GTK_BOX(main_box),color,1,1,1);
+
+    gtk_container_add(GTK_CONTAINER(window), main_box);
+    gtk_widget_show_all(window);
+}
 
 static void set_window (GtkWidget *opcja, gpointer data)
 {
+    gtk_widget_hide(menu);
+
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Order and Chaos");
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    gtk_widget_hide(menu);
-    GtkWidget *test;
-    test = gtk_label_new("a kuku");
-    gtk_container_add(GTK_CONTAINER(window),test);
-    gtk_widget_show_all(window);
-
-    //set_main_area();
+    set_main_area();
 }
 
 static void set_menu (void)
@@ -30,22 +56,22 @@ static void set_menu (void)
 
     gtk_window_set_position(GTK_WINDOW(menu), GTK_WIN_POS_CENTER_ALWAYS);
     menu_box = gtk_box_new(GTK_ORIENTATION_VERTICAL,0);
-    menu_nowa_gra = gtk_button_new_with_label(u8"Nowa gra dla dwóch osób");
-    menu_wczytaj_zapis = gtk_button_new_with_label("Wczytaj grę");
-    menu_komputer = gtk_button_new_with_label("Nowa gra z komputerem");
-    tytul = gtk_label_new("Order and Chaos");
+    menu_new_game = gtk_button_new_with_label(u8"Nowa gra dla dwóch osób");
+    menu_load = gtk_button_new_with_label("Wczytaj grę");
+    menu_ai = gtk_button_new_with_label("Nowa gra z komputerem");
+    title = gtk_label_new("Order and Chaos");
 
-    gtk_box_pack_start(GTK_BOX(menu_box),tytul,1,1,1);
-    gtk_box_pack_start(GTK_BOX(menu_box),menu_nowa_gra,1,1,1);
-    gtk_box_pack_start(GTK_BOX(menu_box),menu_wczytaj_zapis,1,1,1);
-    gtk_box_pack_start(GTK_BOX(menu_box),menu_komputer,1,1,1);
+    gtk_box_pack_start(GTK_BOX(menu_box),title,1,1,1);
+    gtk_box_pack_start(GTK_BOX(menu_box),menu_new_game,1,1,1);
+    gtk_box_pack_start(GTK_BOX(menu_box),menu_load,1,1,1);
+    gtk_box_pack_start(GTK_BOX(menu_box),menu_ai,1,1,1);
 
     gtk_container_add(GTK_CONTAINER(menu),menu_box);
     gtk_widget_show_all(menu);
 
-    g_signal_connect(menu_nowa_gra, "clicked", G_CALLBACK(set_window), "nowa gra");
-    g_signal_connect(menu_wczytaj_zapis, "clicked", G_CALLBACK(set_window), "wczytaj zapis");
-    g_signal_connect(menu_komputer, "clicked", G_CALLBACK(set_window), "komputer");
+    g_signal_connect(menu_new_game, "clicked", G_CALLBACK(set_window), "nowa gra");
+    g_signal_connect(menu_load, "clicked", G_CALLBACK(set_window), "wczytaj zapis");
+    g_signal_connect(menu_ai, "clicked", G_CALLBACK(set_window), "komputer");
 
 
 }
